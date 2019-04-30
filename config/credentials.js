6b74b693549a22a9eb1dd3b58d2993e96ec39652
Joe Schmitz <josephschmitz2018@u.northwestern.edu>
2019-04-29 21:26:51 -0500

    local host running

--- config/credentials.js
+++ config/credentials.js
@@ -1,6 +1,6 @@
 module.exports = {
-  GITHUB_CLIENT_ID: '',
-  GITHUB_CLIENT_SECRET: '',
-  GITHUB_CALLBACK_URL: '',
-  DBURL: '',
+  GITHUB_CLIENT_ID: '630aca5b8028d3c4b084',
+  GITHUB_CLIENT_SECRET: '62dd7a0d99f23a98a32b6f22932a72ecf4625e1b',
+  GITHUB_CALLBACK_URL: 'https://localhost:8080/api/user/authViaGitHub/callback',
+  DBURL: 'mongodb://localhost:27017/reforum',
 };
