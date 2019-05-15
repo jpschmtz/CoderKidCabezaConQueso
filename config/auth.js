45a92748227db64bba4e1cce6d5c890354ba808b
T Saraya <saratgrffn@gmail.com>
2019-05-14 23:31:28 -0500

    passport middleware and server.js

---
+++ config/auth.js
@@ -0,0 +1,15 @@
+module.exports = {
+    ensureAuthenticated: function(req, res, next) {
+      if (req.isAuthenticated()) {
+        return next();
+      }
+      req.flash('error_msg', 'Please log in to view that resource');
+      res.render('I need to change this to the login page using redirect instead of render');
+    },
+    forwardAuthenticated: function(req, res, next) {
+      if (!req.isAuthenticated()) {
+        return next();
+      }
+      res.redirect('/');      
+    }
+  };
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = { 
+    mongoURI: "mongodb+srv://TestUser:12345@cluster0-xoegk.mongodb.net/test?retryWrites=true",    
+};
