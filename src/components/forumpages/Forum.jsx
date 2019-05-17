fb5843500beb5a325cd1753f9f3b8377c33b76b5
Justin (Clark) Jose <44729338+JustinClark94@users.noreply.github.com>
2019-05-16 19:17:03 -0500

    Update Forum.jsx

--- src/components/forumpages/Forum.jsx
+++ src/components/forumpages/Forum.jsx
@@ -8,7 +8,7 @@ import Image from '../../Assets/Forum2.jpg'
 class Forum extends Component {
 	render() {
 		return (
-			<div className="forumPage">
+			<div className="forumContainer">
 
 				<div id="bannerDiv">
 					<div id="intro">
